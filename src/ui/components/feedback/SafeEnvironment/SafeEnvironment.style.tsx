ed1c2d788480586276b8db5d86d18f5ca90e4115
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-06-10 22:06:20 -0300

    SafeEnvironment finalizado

--- src/ui/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
@@ -1,9 +1,10 @@
 import { experimentalStyled as styled } from '@material-ui/core/styles';
 
 export const SafeEnvironmentContainer = styled('div')`
-    //background-color: red;
-    //color: white;
+    
+    color: ${({ theme }) => theme.palette.text.secondary};
+    background-color: ${({ theme }) => theme.palette.background.default};
     text-align: right;
-    padding: 16px 0;
-    font-size: 14px;
+    padding: ${({ theme }) => theme.spacing(2)} 0;
+    font-size: 12px;
 `;
