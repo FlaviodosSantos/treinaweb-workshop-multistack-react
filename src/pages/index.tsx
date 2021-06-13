c9fe387fce2a4b9cb9977f4d07228a635e17ad8a
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-06-13 19:07:54 -0300

    textfiled finalizado, 2a aula até 40m21s

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
+import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask';
 
 
 export default function Home() {
@@ -10,6 +11,10 @@ export default function Home() {
       <PageTitle
         title={'Conheça os profissionais'}
         subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+      />
+
+      <TextFieldMask
+        mask={'99.999-999'} label={'Digite seu CEP'} fullWidth variant={'outlined'}
       />
 
       <UserInformation
